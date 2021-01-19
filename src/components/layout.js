84aba903d36894743d8de0e4713b440e4f8de20f
Stephanie <Rinehart>
2021-01-18 20:31:27 -0500

    add toggle navbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,41 +4,33 @@ import './layout.css';
 import { GithubLogo, InstagramLogo, LinkedinLogo, TwitterLogo } from "phosphor-react";
 
 export default function Layout({ children }) {
-  document.addEventListener('DOMContentLoaded', () => {
+   const [isActive, setisActive] = React.useState(false);
 
-  // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
-  // Check if there are any navbar burgers
-  if ($navbarBurgers.length > 0) {
-
-    // Add a click event on each of them
-    $navbarBurgers.forEach( el => {
-      el.addEventListener('click', () => {
-
-        // Get the target from the "data-target" attribute
-        const target = el.dataset.target;
-        const $target = document.getElementById(target);
-
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-
-      });
-    });
-  }
-
-});
     return (
       <container>
         <section id="home">
+          
           <nav className="navbar is-fixed-top" role="navigation" aria-label="main navigation">
-            <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
-              <span aria-hidden="true"></span>
-              <span aria-hidden="true"></span>
-              <span aria-hidden="true"></span>
-            </a>
-            <div className="navbar-menu" id="navMenu">
+        <div className="navbar-brand">
+          <a
+            onClick={() => {
+              setisActive(!isActive);
+            }}
+            role="button"
+            className={`navbar-burger burger ${isActive ? "is-active" : ""}`}
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarBasicExample"
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+        <div
+          id="navbarBasicExample"
+          className={`navbar-menu ${isActive ? "is-active" : ""}`}
+        >
             <div className="navbar-start is-spaced">
               <div className="navbar-item">   
                 <a href="#home" className="navbar-item">Home</a>
